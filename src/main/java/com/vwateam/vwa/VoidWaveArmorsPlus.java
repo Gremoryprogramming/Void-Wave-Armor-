36d546764d775c2b3b7fac21aca04c30cf0cdb91
zKryle <ciro20797@gmail.com>
2021-11-26 15:02:16 +0100

    Initial Commit

---
+++ src/main/java/com/vwateam/vwa/VoidWaveArmorsPlus.java
@@ -0,0 +1,27 @@
+package com.vwateam.vwa;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(VoidWaveArmorsPlus.MOD_ID)
+public class VoidWaveArmorsPlus
+{
+    // Directly reference a log4j logger.
+    private static final Logger LOGGER = LogManager.getLogger();
+    // Mod ID Const
+    public static final String MOD_ID = "vwa";
+
+    public VoidWaveArmorsPlus() {
+        // Get the mod event bus, so we can listen to events
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+}
---
+++ src/main/java/com/vwateam/vwa/core/Init.java
@@ -0,0 +1,17 @@
+package com.vwateam.vwa.core;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import static com.vwateam.vwa.VoidWaveArmorsPlus.MOD_ID;
+
+public class Init{
+    // ITEM INIT
+    public static DeferredRegister<Item> ITEMS = DeferredRegister.create( ForgeRegistries.ITEMS, MOD_ID );
+
+    // BLOCK INIT
+    public static DeferredRegister<Block> BLOCKS = DeferredRegister.create( ForgeRegistries.BLOCKS, MOD_ID );
+
+}
