057255417af0a08b9014876c26cdd494bfb80188
Gremoryprogramming <91459268+Gremoryprogramming@users.noreply.github.com>
2021-11-27 13:54:17 +0200

    started working on a block
    but my laptop runs out of battery :( anyway tell me if i should change the name...it's current name is "Void Wave Block"

--- src/main/java/com/vwateam/vwa/core/Init.java
+++ src/main/java/com/vwateam/vwa/core/Init.java
@@ -1,7 +1,11 @@
 package com.vwateam.vwa.core;
 
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.material.MaterialColor;
 import net.minecraft.item.Item;
+import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
@@ -13,5 +17,8 @@ public class Init{
 
     // BLOCK INIT
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create( ForgeRegistries.BLOCKS, MOD_ID );
+
+    public static final RegistryObject<Block> VOID_WAVE_BLOCK = BLOCKS.register("void_wave_block",
+            () -> new Block(AbstractBlock.Properties.of(Material.METAL, MaterialColor.COLOR_GRAY).strength(5f, 6f)));
 
 }
