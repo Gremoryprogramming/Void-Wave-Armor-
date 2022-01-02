ab70bb3a7755cf64229ce7c4a869e128fb9c6c44
Gremoryprogramming <91459268+Gremoryprogramming@users.noreply.github.com>
2022-01-02 16:06:27 +0200

    Update VoidWaveBlock.java

--- src/main/java/com/vwateam/vwa/common/block/VoidWaveBlock.java
+++ src/main/java/com/vwateam/vwa/common/block/VoidWaveBlock.java
@@ -1,28 +1,35 @@
 package com.vwateam.vwa.common.block;
 
+import com.vwateam.vwa.core.Init;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.material.MaterialColor;
-import net.minecraft.state.BooleanProperty;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.state.DirectionProperty;
+import net.minecraft.state.IntegerProperty;
 import net.minecraft.state.StateContainer;
+import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Direction;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.world.World;
 
 public class VoidWaveBlock extends Block {
-    public static final BooleanProperty BLOCKSTATE_CHANGE = BooleanProperty.create("blockstate");
+    public static final IntegerProperty WAVE_CRYSTAL_LEVEL = IntegerProperty.create( "wave_crystal_level", 0, 4);
     public static final DirectionProperty FACING = DirectionProperty.create("facing", Direction.Plane.HORIZONTAL);
 
     public VoidWaveBlock() {
         super(AbstractBlock.Properties.of(Material.METAL, MaterialColor.COLOR_GRAY).strength(15).sound(SoundType.METAL));
-        registerDefaultState(this.defaultBlockState().setValue(BLOCKSTATE_CHANGE, false).setValue(FACING, Direction.NORTH));
+        registerDefaultState(this.defaultBlockState().setValue(WAVE_CRYSTAL_LEVEL, 0).setValue(FACING, Direction.NORTH));
     }
 
     @Override
     protected void createBlockStateDefinition(StateContainer.Builder<Block, BlockState> builder) {
-        builder.add(FACING, BLOCKSTATE_CHANGE);
+        builder.add(FACING, WAVE_CRYSTAL_LEVEL);
         super.createBlockStateDefinition(builder);
     }
 
@@ -32,4 +39,13 @@ public class VoidWaveBlock extends Block {
         return true;
     }
 
+    @Override
+    public ActionResultType use(BlockState pState, World pLevel, BlockPos pPos, PlayerEntity pPlayer, Hand pHand, BlockRayTraceResult pHit) {
+        if (pPlayer.getItemInHand(pHand).getItem() == Init.WAVE_CRYSTAL.get() && pState.getValue(WAVE_CRYSTAL_LEVEL) < 4 ) {
+            pPlayer.getItemInHand(pHand).shrink(1);
+            pLevel.setBlockAndUpdate(pPos, pState.setValue(WAVE_CRYSTAL_LEVEL, pState.getValue(WAVE_CRYSTAL_LEVEL) + 1));
+            return ActionResultType.SUCCESS;
+        }
+        return ActionResultType.PASS;
+    }
 }
